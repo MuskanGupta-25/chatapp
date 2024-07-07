45a02c7a64c5e53668a6c268cb0a6d3ff854afd7
MuskanGupta-25 <muskangupta15501@gmail.com>
2024-07-07 14:50:52 +0530

    Usersidebar backend completed

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
+//import packages
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+//Routes
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
+
+
 import connectToMongoDB from './db/connectToMongoDB.js';
-import cookieParser from 'cookie-parser';
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -15,6 +22,8 @@ app.use(cookieParser());
 // routes
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes);
+
 
 
 // app.get("/",(req,res)=>{
