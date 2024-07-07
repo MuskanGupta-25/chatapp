3f39af0d79bf44064c1cbf73887e9a18083249f8
MuskanGupta-25 <muskangupta15501@gmail.com>
2024-07-07 14:54:35 +0530

    usercontrollerandroutes backend completed

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 //Routes
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
-import userRoutes from './routes/user.routes.js';
+
 
 
 import connectToMongoDB from './db/connectToMongoDB.js';
@@ -22,7 +22,7 @@ app.use(cookieParser());
 // routes
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
-app.use("/api/users",userRoutes);
+
 
 
 
