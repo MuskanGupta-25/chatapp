dd9b4192df4457c44d4a886f43f9ca72e994f2a7
MuskanGupta-25 <muskangupta15501@gmail.com>
2024-07-10 23:48:15 +0530

    signup functionality completed

--- frontend/src/pages/singup/GenderCheckbox.jsx
+++ frontend/src/pages/singup/GenderCheckbox.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
+import React from 'react';
 
-const GenderCheckbox = () => {
+const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
   return (
     <div className='flex'>
-        <div className='form-control'>
-            <label className={`label gap-1 cursor-pointer`}>
-                <span className='label-text text-white p-2 text-lg'>Male</span>
-                <input type="checkbox" defaultChecked className="checkbox border-stone-500" />
-            </label>
-        </div>
-        
-        <div className='form-control'>
-            <label className={`label gap-1 cursor-pointer`}>
-                <span className='label-text text-white p-2 text-lg'>Female</span>
-                <input type="checkbox" defaultChecked className="checkbox border-stone-500" />
-            </label>
-        </div>
+      <div className='form-control'>
+        <label className={`label gap-1 cursor-pointer ${selectedGender === "male" ? "selected" : ""}`}>
+          <span className='label-text text-white p-2 text-lg'>Male</span>
+          <input
+            type="checkbox"
+            className="checkbox border-stone-500"
+            checked={selectedGender === "male"}
+            onChange={() => onCheckboxChange("male")}
+          />
+        </label>
+      </div>
+      
+      <div className='form-control'>
+        <label className={`label gap-1 cursor-pointer ${selectedGender === "female" ? "selected" : ""}`}>
+          <span className='label-text text-white p-2 text-lg'>Female</span>
+          <input
+            type="checkbox"
+            className="checkbox border-stone-500"
+            checked={selectedGender === "female"}
+            onChange={() => onCheckboxChange("female")}
+          />
+        </label>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default GenderCheckbox;
+
 
 // started code for gender component
 // import React from 'react'
