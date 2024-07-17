c8007f5b91653ba62451927fcae76fc82fae9ebe
MuskanGupta-25 <muskangupta15501@gmail.com>
2024-07-17 18:37:24 +0530

    get msg completed

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,17 +1,14 @@
 import User from "../models/user.model.js";
 
-export const getUsersForSidebar = async(req,res)=>{
-    try {
-        const loggedInUserId = req.user._id
+export const getUsersForSidebar = async (req, res) => {
+	try {
+		const loggedInUserId = req.user._id;
 
-        //to get all the user on sidebar we user this all user means the login one also means to chat yourself
-        // const allUsers = await User.find()
+		const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
-        // to get all the user except the login in one we do filter
-        const filterUsers = await User.find({id: {$ne:loggedInUserId}}).select("-password").select("-createdAt").select("-updatedAt")
-        res.status(200).json(filterUsers);
-    } catch (error) {
-        console.error("Error in getUsersForSidebar:", error.message)
-        res.status(500).json({error: "Internal server error"})
-    }
+		res.status(200).json(filteredUsers);
+	} catch (error) {
+		console.error("Error in getUsersForSidebar: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
 };
--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,41 +1,30 @@
-import React from 'react'
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utilis/extractTime";
+import useConversation from "../../zustand/useConversation";
 
-const Message = () => {
-  return (
-    <div className='chat chat-end'>
-      <div className='chat-image avatar'>
-        <div className='w-10 rounded-full'>
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Tailwind CSS chat bubble component"  />
-        </div>
-      </div>
-      <div className={`chat-bubble text-white bg-stone-700`}>
-        Hi! What is Upp?
-      </div>
-      <div className='chat-footer opacity-50 text-xs flex gap-1 items-center text-black'>12:42</div>
-    </div>
-  )
-}
+const Message = ({ message }) => {
+	const { authUser } = useAuthContext();
+	const { selectedConversation } = useConversation();
+	const fromMe = message.senderId === authUser._id;
+	const formattedTime = extractTime(message.createdAt);
+	const chatClassName = fromMe ? "chat-end" : "chat-start";
+	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+	const bubbleBgColor = fromMe ? "bg-stone-600" : "";
 
-export default Message
+	
 
+	return (
+		<div className={`chat ${chatClassName}`}>
+			<div className='chat-image avatar'>
+				<div className='w-10 rounded-full'>
+					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
+				</div>
+			</div>
 
-// starter code for this file
-// import React from 'react'
-
-// const Message = () => {
-//   return (
-//     <div className='chat chat-end'>
-//       <div className='chat-image avatar'>
-//         <div className='w-10 rounded-full'>
-//             <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Tailwind CSS chat bubble component"  />
-//         </div>
-//       </div>
-//       <div className={`chat-bubble text-white bg-stone-700`}>
-//         Hi! What is Upp?
-//       </div>
-//       <div className='chat-footer opacity-50 text-xs flex gap-1 items-center text-black'>12:42</div>
-//     </div>
-//   )
-// }
-
-// export default Message
+      
+			<div className={`chat-bubble text-white  ${bubbleBgColor} pb-2  `}>{message.message}</div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
+		</div>
+	);
+};
+export default Message;
