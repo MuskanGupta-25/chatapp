1ccaf84425c1df7a75eb96523361d51369ace834
MuskanGupta-25 <muskangupta15501@gmail.com>
2024-07-18 18:37:41 +0530

    updates

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,13 +1,10 @@
-import express from 'express';
-import { getMessages, sendMessage } from '../controllers/message.controller.js';
-import protectRoute from '../middleware/protectRoute.js';
+import express from "express";
+import { getMessages, sendMessage } from "../controllers/message.controller.js";
+import protectRoute from "../middleware/protectRoute.js";
+
 const router = express.Router();
 
-//get message b/w two user
-router.get("/:id",protectRoute,getMessages);
-
-router.post("/send/:id",protectRoute,sendMessage);
-//protectRoutes -  is like a authorization process  or we can say that user is login or not
-
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
